2854d643571678cbbac42584666acdd771babd7d
Garrett O'Hara <garrett.ohara@nbcuni.com>
2024-05-07 18:08:26 -0700

    adding CIDR

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -18,6 +18,8 @@ module "s3_storage" {
 module "garrett_server" {
   source        = "../../terraform/web_server"
   aws_bucket_id = module.s3_storage.aws_s3_bucket_id
-  cidr_blocks   = ["10.57.0.0/16"]
-  tags          = module.tags.tags
+
+  # NBC VPN CIDR range
+  cidr_blocks = ["10.57.0.0/16"]
+  tags        = module.tags.tags
 }
