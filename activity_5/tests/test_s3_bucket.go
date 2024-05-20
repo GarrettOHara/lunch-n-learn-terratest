2c43d761dd71dd0baa9403d9d379b11e0b5414c0
Garrett O'Hara <garrett.ohara@nbcuni.com>
2024-05-20 14:21:55 -0700

    formatting

--- activity_5/tests/test_s3_bucket.go
+++ activity_5/tests/test_s3_bucket.go
@@ -1,4 +1,3 @@
-
 package test
 
 import (
@@ -18,28 +17,28 @@ func TestS3Bucket(t *testing.T) {
 	// Use the random ID and terratest prefix to generate a random name
 	name := fmt.Sprintf("terratest-%s", randomID)
 
-    // Use the CopyTerraformFolderToTemp function to generate a randomly
-    // named directory for holding the root-level module/state.
-    //
+	// Use the CopyTerraformFolderToTemp function to generate a randomly
+	// named directory for holding the root-level module/state.
+	//
 	// Enables us to deploy multiple clusters from the same root-level
-    // configuration with different parameters in parallel.
+	// configuration with different parameters in parallel.
 	workingDir := test_structure.CopyTerraformFolderToTemp(t, "..", "examples/complete")
 
-    testName := t.Name()
-    bucketName := fmt.Sprintf("terratest-lunch-n-learn/%s/us-west-1/%s.tfstate", testName, name)
-    terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+	testName := t.Name()
+	bucketName := fmt.Sprintf("terratest-lunch-n-learn/%s/us-west-1/%s.tfstate", testName, name)
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: workingDir,
 		BackendConfig: map[string]interface{}{
 			"key": bucketName,
 		},
 		Vars: map[string]interface{}{
-			"name":   name,
+			"name": name,
 		},
 		NoColor: true,
-    })
-    defer terraform.Destroy(t, terraformOptions)
+	})
+	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
 
 	s3BucketARN := terraform.Output(t, terraformOptions, "s3_bucket_arn")
-    assert.NotEmpty(t, s3BucketARN, "S3 bucket ARN should not be empty")
+	assert.NotEmpty(t, s3BucketARN, "S3 bucket ARN should not be empty")
 }
---
+++ activity_5/tests/test_web_server.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestS3Bucket(t *testing.T) {
+	// Generate a 6-character random string
+	randomID := random.UniqueId()
+	// Use the random ID and terratest prefix to generate a random name
+	name := fmt.Sprintf("terratest-%s", randomID)
+
+	// Use the CopyTerraformFolderToTemp function to generate a randomly
+	// named directory for holding the root-level module/state.
+	//
+	// Enables us to deploy multiple clusters from the same root-level
+	// configuration with different parameters in parallel.
+	workingDir := test_structure.CopyTerraformFolderToTemp(t, "..", "examples/complete")
+
+	testName := t.Name()
+	bucketName := fmt.Sprintf("terratest-lunch-n-learn/%s/us-west-1/%s.tfstate", testName, name)
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: workingDir,
+		BackendConfig: map[string]interface{}{
+			"key": bucketName,
+		},
+		Vars: map[string]interface{}{
+			"name": name,
+		},
+		NoColor: true,
+	})
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+
+	s3BucketARN := terraform.Output(t, terraformOptions, "s3_bucket_arn")
+	assert.NotEmpty(t, s3BucketARN, "S3 bucket ARN should not be empty")
+}
