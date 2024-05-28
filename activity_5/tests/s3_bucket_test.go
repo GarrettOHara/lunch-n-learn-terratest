dfa5780d4f7eef8b969994a3d7bfa62510d6dfb5
Garrett O'Hara <garrett.ohara@nbcuni.com>
2024-05-28 11:35:25 -0700

    updated test with error checking

--- activity_5/tests/s3_bucket_test.go
+++ activity_5/tests/s3_bucket_test.go
@@ -2,20 +2,20 @@ package test
 
 import (
 	"fmt"
-    "io"
-    "net/http"
-    "strings"
+	"io"
+	"net/http"
+	"strings"
 	"testing"
 
-    "github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestS3Bucket(t *testing.T) {
-    // Allow test to run in parrallel with other tests
-    t.Parallel()
+	// Allow test to run in parallel with other tests
+	t.Parallel()
 
 	// Use the CopyTerraformFolderToTemp function to generate a randomly
 	// named directory for holding the root-level module/state.
@@ -24,17 +24,21 @@ func TestS3Bucket(t *testing.T) {
 	// configuration with different parameters in parallel.
 	workingDir := test_structure.CopyTerraformFolderToTemp(t, "..", "examples/storage")
 
-    defer test_structure.RunTestStage(t, "destroy_infrastructure", func() {
+	defer test_structure.RunTestStage(t, "destroy_infrastructure", func() {
 		DestroyInfrastructure(t, workingDir)
 	})
 
-    test_structure.RunTestStage(t, "s3_bucket_check", func() {
-        S3BucketCheck(t, workingDir)
-    })
+	test_structure.RunTestStage(t, "deploy_infrastructure", func() {
+		DeployInfrastructure(t, workingDir)
+	})
 
-    test_structure.RunTestStage(t, "static_website_response", func() {
-        StaticWebsiteResponse(t, workingDir)
-    })
+	test_structure.RunTestStage(t, "s3_bucket_check", func() {
+		S3BucketCheck(t, workingDir)
+	})
+
+	test_structure.RunTestStage(t, "static_website_response", func() {
+		StaticWebsiteResponse(t, workingDir)
+	})
 }
 
 // Stage 1: DeployInfrastructure
@@ -59,10 +63,13 @@ func DeployInfrastructure(t *testing.T, workingDir string) {
 		},
 		NoColor: true,
 	})
+    // Save the options and key for later test stages to use
+	test_structure.SaveTerraformOptions(t, workingDir, terraformOptions)
+
 	// run "terraform apply"
 	fmt.Println("Running 'terraform init' and 'terraform apply'...")
 	terraform.InitAndApply(t, terraformOptions)
-    fmt.Println("Terraform apply complete.")
+	fmt.Println("Terraform apply complete.")
 }
 
 // Stage 2: S3BucketCheck
@@ -70,7 +77,7 @@ func DeployInfrastructure(t *testing.T, workingDir string) {
 // and tests to see if its a non empty string which determines if the
 // S3 bucket exists.
 func S3BucketCheck(t *testing.T, workingDir string) {
-    terraformOptions := test_structure.LoadTerraformOptions(t, workingDir)
+	terraformOptions := test_structure.LoadTerraformOptions(t, workingDir)
 	s3BucketId := terraform.Output(t, terraformOptions, "s3_bucket_id")
 	assert.NotEmpty(t, s3BucketId, "S3 bucket ID should not be empty")
 }
@@ -79,34 +86,39 @@ func S3BucketCheck(t *testing.T, workingDir string) {
 // This function grabs the S3 Static Website Endpoint from the module output
 // and contructs an HTTP GET request to see if the static content is reachable.
 func StaticWebsiteResponse(t *testing.T, workingDir string) {
-    terraformOptions := test_structure.LoadTerraformOptions(t, workingDir)
-    s3StaticSiteURL := terraform.Output(t, terraformOptions, "website_endpoint")
-    assert.NotEmpty(t, s3StaticSiteURL, "S3 static website endpoint should not be empty")
+	terraformOptions := test_structure.LoadTerraformOptions(t, workingDir)
+	s3StaticSiteURL := terraform.Output(t, terraformOptions, "website_endpoint")
+	assert.NotEmpty(t, s3StaticSiteURL, "S3 static website endpoint should not be empty")
 
-    // Send GET request
-    resp, err := http.Get(s3StaticSiteURL)
-    if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
-    defer resp.Body.Close()
+	// Send GET request
+    resp, err := http.Get("http://"+s3StaticSiteURL)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
 
-    // Check response status code
-    if resp.StatusCode != http.StatusOK {
-        fmt.Println("Unexpected status code:", resp.StatusCode)
-        return
-    }
+	// Check response status code
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Unexpected status code:", resp.StatusCode)
+		return
+	}
 
-    // Read response body
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        fmt.Println("Error reading response body:", err)
-        return
-    }
+	// Read response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+    fmt.Printf("Server response code: %d\n", resp.StatusCode)
+	fmt.Println("Response from server:")
+	fmt.Println(string(body))
+    serverResponse := strings.ReplaceAll(string(body), "\n", "")
+    serverResponse = strings.ReplaceAll(serverResponse, " ", "")
+    expectedHTML := `<html><body><p>OK</p></body></html>`
 
-    // Print response body
-    fmt.Println("Response body:")
-    fmt.Println(string(body))
+    assert.Equal(t, resp.StatusCode, http.StatusOK, "The response from the server was not 200.")
+    assert.Equal(t, serverResponse, expectedHTML, "The response from the server did not match the expected result.")
 }
 
 // Stage 4: DestroyInfrastructure
